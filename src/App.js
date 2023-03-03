23492d631042aed1217ab45b31734ba9b0337a0b
Debolu <obayemiadebolu16@gmail.com>
2023-03-03 21:05:16 +0100

    render functional menu component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+const [ menuItems, setMenuItems ] = useState(items);
+// const[ categories, setCategories ] = useState([]);
+
+const filterItems = (category) => {
+  const newItems = items.filter((item) => item.category === category)
+  setMenuItems(newItems)
+  if (category === "all") {
+    setMenuItems(items)
+  }
+}
+
+  return <main>
+    <section className="menu section">
+      <div className="title">
+        <h2 className="title">Our Menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Categories filterItems = {filterItems} menuItems = {menuItems} />
+      <Menu menuItems = {menuItems}/>
+    </section>
+  </main>;
 }
 
 export default App;
