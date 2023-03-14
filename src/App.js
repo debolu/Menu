9461c03e4f69085cdf163003a5164131e1b89a7a
Debolu <obayemiadebolu16@gmail.com>
2023-03-14 12:53:30 +0100

    update the categories dynamically

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const categories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
-const [ menuItems, setMenuItems ] = useState(items);
-// const[ categories, setCategories ] = useState([]);
+  const [menuItems, setMenuItems] = useState(items);
 
-const filterItems = (category) => {
-  const newItems = items.filter((item) => item.category === category)
-  setMenuItems(newItems)
-  if (category === "all") {
-    setMenuItems(items)
-  }
-}
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+    if (category === "all") {
+      setMenuItems(items);
+    }
+  };
 
-  return <main>
-    <section className="menu section">
-      <div className="title">
-        <h2 className="title">Our Menu</h2>
-        <div className="underline"></div>
-      </div>
-      <Categories filterItems = {filterItems} menuItems = {menuItems} />
-      <Menu menuItems = {menuItems}/>
-    </section>
-  </main>;
+  console.log(categories)
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2 className="title">Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories
+          categories={categories}
+          filterItems={filterItems}
+          menuItems={menuItems}
+        />
+        <Menu menuItems={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- src/Categories.js
+++ src/Categories.js
@@ -1,12 +1,29 @@
 import React from "react";
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
-      <button className="filter-btn" onClick={() => filterItems("all")}>All</button>
-      <button className="filter-btn" onClick={() => filterItems("breakfast")}>Breakfast</button>
-      <button className="filter-btn" onClick={() => filterItems("lunch")}>Lunch</button>
-      <button className="filter-btn" onClick={() => filterItems("shakes")}>Shakes</button>
+      { categories.map((category, index) => {
+        return (
+          <div key={index}>
+          <button
+            
+            type="button"
+            className="filter-btn"
+            onClick={() => {filterItems(category);}}
+          >
+            {category}
+          </button>
+          </div>
+        );
+      }) }
+
+{/* correct the code above to replace the buttons  below to make the buttons render dynamically
+ based on the numer of categories present in the data */}
+      {/* <button className="filter-btn" onClick={() => {filterItems('all');}}>All</button>
+      <button className="filter-btn" onClick={() => {filterItems('breakfast');}}>Breakfast</button>
+      <button className="filter-btn" onClick={() => {filterItems('lunch');}}>Lunch</button>
+      <button className="filter-btn" onClick={() => {filterItems('shakes');}}>Shakes</button> */}
     </div>
   );
 };
